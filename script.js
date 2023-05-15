0a60cec90b34db7141192dde03a5148713490cef
Eduardo Backese <eduardo.programacao@hotmail.comm>
2023-05-15 00:24:41 -0300

    first

---
+++ script.js
@@ -0,0 +1,29 @@
+function vermelho(){
+    var pegandoVermelho = document.querySelector("body");
+
+    pegandoVermelho.classList.add("red")
+}
+
+function branco(){
+    var pegandoBranco = document.querySelector("body");
+
+    pegandoBranco.classList.add("white");
+}
+
+function azul(){
+    var pegandoAzul = document.querySelector("body");
+
+    pegandoAzul.classList.add("blue");
+}
+
+function laranja(){
+    var pegandoAzul = document.querySelector("body");
+
+    pegandoAzul.classList.add("orange");
+}
+
+function preto(){
+    var pegandoPreto = document.querySelector("body");
+
+    pegandoPreto.classList.add("black");
+}
